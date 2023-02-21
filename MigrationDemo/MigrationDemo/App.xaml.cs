1641beeee51dff61d301ff6d7f2e33cf73ccc620
André Krämer <andre.kraemer@qualitybytes.de>
2023-02-21 19:57:23 +0100

    Projektdateien hinzufügen.

---
+++ MigrationDemo/MigrationDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MigrationDemo.Services;
+using MigrationDemo.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MigrationDemo
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MigrationDemo/MigrationDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MigrationDemo.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MigrationDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
